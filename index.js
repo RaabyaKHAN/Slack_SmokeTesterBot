f833d5b77f6b4f220265a4e3a986cc2473505ea1
Chance Shiflett <chanceshiflett@yahoo.com>
2018-10-10 11:28:54 -0700

    Add emily and pat

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const Slackbot = require ('slackbots');
 const axios = require ('axios');
-var qaMembers = ['chance', 'kaygee', 'raabya', 'james'];
+var qaMembers = ['Chance', 'Kaygee', 'Raabya', 'James', 'Emily', 'Pat'];
 const bot = new Slackbot({
-    token: 'xoxb-2670620364-398066704102-o0W7SYki1zAK00tfNGJYah4h',
-    name: 'SmokeTesterBot'
+   token: 'xoxb-2670620364-398066704102-o0W7SYki1zAK00tfNGJYah4h',
+   name: 'SmokeTesterBot'
 });
 
 //Start Handler
 bot.on('start', function(){
-     bot.postMessageToChannel(
-     'test123', 
-     'Get ready to smoke test. Here are your assignments for the week \n'+assignMembers(),
-     ':fire:'
-     );
- }
+    bot.postMessageToChannel(
+    'qa-team',
+    'Get ready to smoke test. Here are your assignments for the week \n'+assignMembers(),
+    ':fire:'
+    );
+}
 );
 
 //Error Handler
@@ -21,60 +21,62 @@ bot.on('error',(err)=> console.log(err));
 
 //Message Handler
 bot.on('message', data => {
-    if (data.type !== 'message') {
-      return;
-    }
-  
-    handleMessage(data.text);
-  });
-  
-  // Respons to Data
-  function handleMessage(message) {
-    if (message.includes('addmember')) {
-        bot.postMessageToChannel('test123', 'Who is the new QA team Member?')
-        var member = bot.getUser();
-            addMember(member.text);
-            bot.postMessage('testforraabya','Ok I have added '+member.text+' to the member list')
-            bot.postMessage('testforraabya','This is the current list of members')
-    } else if (message.includes('removemember')) {
-      removeMember()
-      bot.postMessageToChannel('test123', 'Who is leaving us?')
-      data => {
-        removeMember(data.text);
-      }
-    } 
-  }
+   if (data.type !== 'message') {
+     return;
+   }
+
+   handleMessage(data.text);
+ });
+
+ // Respons to Data
+ function handleMessage(message) {
+   if (message.includes('addmember')) {
+       bot.postMessageToChannel('qa-team', 'Who is the new QA team Member?')
+       var member = bot.getUser();
+           addMember(member.text);
+           bot.postMessage('testforraabya','Ok I have added '+member.text+' to the member list')
+           bot.postMessage('testforraabya','This is the current list of members')
+   } else if (message.includes('removemember')) {
+     removeMember()
+     bot.postMessageToChannel('qa-team', 'Who is leaving us?')
+     data => {
+       removeMember(data.text);
+     }
+   }
+ }
 function assignMembers(){
-    var areaForSmokeTesting = ['Freelancer Application TC , Revver Tools TC, CTCE',
-    'Freelancer Application CP, Revver Tools CP, CCPE',
-    'Freelancer Application SU, Revver Tools SU, Customer Editor SU',
-    'Freelancer Application TR, Revver Tools TR, Customer Order details TR, Temi, Checkout -> (Prepay + Invoicing + Credit limit, Credit card, Paypal'];
-    qaMembers = shuffleArray (qaMembers);
-    var temp='';
-    for (var i = qaMembers.length-1; i >=0; i--)
-    temp = temp + '@'+qaMembers[i]+' : '+ areaForSmokeTesting[i] +'\n'; 
-    return temp;
+   var areaForSmokeTesting =
+   ['Translator Application, Tranlation job claim, Translator customer side, Workspace claim jobs (TR, CP, SU, TR)',
+   'Line Editor (SR, Non-SR), CTCE timestamped (Desktop + Mobil), CTCE non-timestamped (Desktop + Mobil)',
+   'Dash, CPCE (Desktop + Mobil), SU Editor, SU Customer editor (Desktop + Mobil)',
+   'Freelancer CP, Freelancer SU, Freelancer TC',
+   'Temi set up order (Mobil + Dekstop), Temi Editor (Mobil + Dekstop)',
+   'QC & Grading, Customer Experience: Payment & Billing, My Files & Search Files, Order Hitory'];
+   qaMembers = shuffleArray (qaMembers);
+   var temp='';
+   for (var i = qaMembers.length-1; i >=0; i--)
+   temp = temp + '@'+qaMembers[i]+' : '+ areaForSmokeTesting[i] +'\n';
+   return temp;
 }
 
 function shuffleArray (array){
-   for (var i = array.length-1; i >=0; i--) {
-     
-        var randomIndex = Math.floor(Math.random()*(i+1)); 
-        var itemAtIndex = array[randomIndex]; 
-         
-        array[randomIndex] = array[i]; 
-        array[i] = itemAtIndex;
-    }
-    
-    return array
+  for (var i = array.length-1; i >=0; i--) {
+
+       var randomIndex = Math.floor(Math.random()*(i+1));
+       var itemAtIndex = array[randomIndex];
+       array[randomIndex] = array[i];
+       array[i] = itemAtIndex;
+   }
+
+   return array
 }
 
 function addMember(newMember)
 {
-    qaMembers.push(newMember);
+   qaMembers.push(newMember);
 }
 
 function removeMember(memberToRemove)
 {
-    qaMembers.splice(qaMembers.findIndex(memberToRemove),1)
+   qaMembers.splice(qaMembers.findIndex(memberToRemove),1)
 }
